bd535214a819c70f5267cbb24325ed572d381761
Daniele Kleven <98479520+danikleven@users.noreply.github.com>
2026-03-03 14:38:05 +0100

    Organizing project structure: separate frontend and backend

---
+++ Backend/src/test/java/com/kleven/Readly/ReadlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kleven.Readly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
